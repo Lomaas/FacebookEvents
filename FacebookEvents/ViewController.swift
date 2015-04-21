bcf9339c5169b9ae1099e0bc49f175f97bde5458
Simen Johannessen <simenlomaas@gmail.com>
2015-04-21 19:58:26 +0200

    Initial Commit

---
+++ FacebookEvents/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FacebookEvents
+//
+//  Created by Simen Johannessen on 21/04/15.
+//  Copyright (c) 2015 Bekk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
