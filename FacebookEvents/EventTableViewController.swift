db2ce66120e09c535ba475a1532ff2b6036c03e5
Simen Johannessen <simenlomaas@gmail.com>
2015-04-22 23:33:47 +0200

    refactoring

--- FacebookEvents/EventTableViewController.swift
+++ FacebookEvents/EventTableViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class EventTableViewController: UITableViewController {
+class EventsTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
