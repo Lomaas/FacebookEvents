da201a80b84ee3bf53c8e8426af91c251221d6dd
Simen Johannessen <simenlomaas@gmail.com>
2015-04-23 23:34:25 +0200

    fixed facebook login and refactoring2

--- FacebookEvents/EventTableViewController.swift
+++ FacebookEvents/EventTableViewController.swift
@@ -9,7 +9,12 @@
 import UIKit
 
 class EventsTableViewController: UIViewController {
-
+    let eventsMock = [
+        (title: "Salg på Komplett.no", eventDescription: "Crazy Weekend på Komplett.no med helt sinnsyke priser!"),
+        (title: "Tom Waits-løpet 2015", eventDescription: "Hvert år møter over tusen forventningsfulle osloborgere opp i Middelalderparken for å delta på Norges største pub-crawl «Tom Waits-løpet»"),
+        (title: "Oslo Karneval 2015", eventDescription: "Oslo Karneval holdes 6. juni, i samarbeid med Musikkfest Oslo!")
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -19,16 +24,28 @@ class EventsTableViewController: UIViewController {
         
         if FBSDKAccessToken.currentAccessToken() == nil {
             println("Start login with Facebook Service")
-
-            LoginWithFacebookService.login({ () -> Void in
-                // Fetch events here
-            }, errorHandler: { (error) -> Void in
-                // Display error for user
-            })
+           login()
         }
         else {
-            println("Is already signed in")
+            println("Is already signed in. Do something, get data?")
         }
+    }
+    
+    func login() {
+        let manger = FBSDKLoginManager()
+        let permissions = ["public_profile", "email"]
+        
+        manger.logInWithReadPermissions(permissions, handler: { (result, error) -> Void in
+            if error != nil {
+                println("Error, \(error.localizedDescription)")
+            }
+            if result.isCancelled {
+                println("User cancelled \(result)")
+            }
+            else {
+                println("Successfully granted. Do something, get data?")
+            }
+        })
     }
 }
 
